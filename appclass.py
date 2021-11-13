28d1b72f84945bdd4efb9f616bfa584bfac9af6f
Wallace Wong <wallace.w1997@gmail.com>
2021-11-13 04:51:17 -0500

    refactor to make widgets it's own functions to reuse

--- appclass.py
+++ appclass.py
@@ -4,6 +4,7 @@ from typing import _SpecialForm
 from PySide6 import QtCore, QtWidgets, QtGui
 from PySide6.QtGui import QCursor, QPixmap
 from PySide6.QtWidgets import QGridLayout, QLabel, QPushButton
+from shiboken6.Shiboken import Object
 
 class ArtHiest(QtWidgets.QWidget):
     def __init__(self):
@@ -15,21 +16,35 @@ class ArtHiest(QtWidgets.QWidget):
         grid = QGridLayout()
 
         # Logo Display
-        image = QPixmap("arthiesttransp.png")
-        logo = QLabel( objectName = 'logo')
-        logo.setPixmap(image)
-        logo.setAlignment(QtCore.Qt.AlignCenter)
-        grid.addWidget(logo, 1, 0)
+        logo = self.logo("arthiesttransp.png", "logo")
+        grid.addWidget(logo, 0, 0)
 
         #button widget
-        btn = QPushButton("Button", objectName="btn")
-        btn.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-        grid.addWidget(btn, 2, 0)
-
+        btn = self.btn("Browse", "btn")
+        grid.addWidget(btn, 0, 1)
         
+        grid.setRowStretch(1,1)
+
         self.setLayout(grid)
+    
+    # quick logo creation
+    def logo(self, imagelink, objectname = ""):
+        image = QPixmap(imagelink)
+        image = image.scaledToHeight(100)
+        logo = QLabel(objectname, objectName = objectname) if objectname else QLabel(objectname)
+        logo.setPixmap(image)
+        logo.setAlignment(QtCore.Qt.AlignCenter)
+        return logo
 
+    # quick button creation
+    def btn(self, text, objectname = "None"):
+        btn = QPushButton(text, ObjectName = objectname) if objectname else QPushButton(text)
+        btn.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+        return btn
 
+    def label(self, text):
+        label = QLabel(text)
+        return label
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     stylesheet = "./style.css"
@@ -38,7 +53,7 @@ if __name__ == "__main__":
         app.setStyleSheet(fh.read())
 
     widget = ArtHiest()
-    widget.resize(600, 500)
+    # widget.resize(600, 500)
     widget.show()
 
     sys.exit(app.exec())
